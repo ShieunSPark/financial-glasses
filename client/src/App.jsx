8315c89d6f737abe6e2e972db5e37a7954066221
Shieun Park <121051962+ShieunSPark@users.noreply.github.com>
2023-12-04 18:24:21 -0500

    Cleaning up home page (frontend)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import logo from "./assets/fgLogo.svg";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col h-screen justify-center items-center p-5">
+      <img src={logo} className="w-48" alt="Financial Glasses logo" />
+      <h1 className="animate-pulse text-5xl p-2">Financial Glasses</h1>
+      <h3 className="p-2">What Mint wished it could be</h3>
       <div>
-        <img src={logo} className="logo" alt="Financial Glasses logo" />
+        <button className="transition ease-in-out delay-50 hover:scale-110 hover:bg-indigo-500 duration-100 rounded-md bg-blue-500 p-2 m-2">
+          Sign Up
+        </button>
+        <button className="transition ease-in-out delay-50 hover:scale-110 hover:bg-indigo-500 duration-100 rounded-md bg-blue-500 p-2 m-2">
+          Log In
+        </button>
       </div>
-      <h1 className="text-2xl text-center underline text-blue-500">
-        Financial Glasses
-      </h1>
-      <h3 className="">What Mint wished it could be</h3>
-    </>
+    </div>
   );
 }
 
