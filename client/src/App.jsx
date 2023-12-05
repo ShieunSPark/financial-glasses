f3a2f28104261e0ce80cedc5a5233cd2e7d3a76f
Shieun Park <121051962+ShieunSPark@users.noreply.github.com>
2023-12-05 11:08:05 -0500

    Set up Login/SignUp pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,40 @@
-import logo from "./assets/fgLogo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Error from "./routes/Error";
+import Home from "./routes/Home";
+import Login from "./routes/LogIn";
+import SignUp from "./routes/SignUp";
 
 function App() {
-  return (
-    <div className="flex flex-col h-screen justify-center items-center p-5">
-      <img src={logo} className="w-48" alt="Financial Glasses logo" />
-      <h1 className="animate-pulse text-5xl p-2">Financial Glasses</h1>
-      <h3 className="p-2">What Mint wished it could be</h3>
-      <div>
-        <button className="transition ease-in-out delay-50 hover:scale-110 hover:bg-indigo-500 duration-100 rounded-md bg-blue-500 p-2 m-2">
-          Sign Up
-        </button>
-        <button className="transition ease-in-out delay-50 hover:scale-110 hover:bg-indigo-500 duration-100 rounded-md bg-blue-500 p-2 m-2">
-          Log In
-        </button>
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <Error />,
+      children: [
+        // {
+        //   path: "about",
+        //   element: <About />,
+        // },
+        // {
+        //   path: "blog",
+        //   element: <Blog data={data} />,
+        // },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
+      errorElement: <Error />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+import logo from "../assets/fgLogo.svg";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center p-5">
+      <img src={logo} className="w-48" alt="Financial Glasses logo" />
+      <h1 className="animate-pulse text-5xl p-2">Financial Glasses</h1>
+      <h3 className="p-2">What Mint wished it could be</h3>
+      <div>
+        <button className="transition ease-in-out delay-50 hover:scale-110 hover:bg-indigo-500 duration-100 rounded-md bg-blue-500 p-2 m-2">
+          <Link to={`signup`}>Sign Up</Link>
+        </button>
+        <button className="transition ease-in-out delay-50 hover:scale-110 hover:bg-indigo-500 duration-100 rounded-md bg-blue-500 p-2 m-2">
+          <Link to={`login`}>Log In</Link>
+        </button>
+      </div>
+    </div>
+  );
+}
