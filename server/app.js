7b8bf6fc00e5630f0bf5e50269a81f8a2ad74dea
Shieun <121051962+ShieunSPark@users.noreply.github.com>
2025-02-13 15:52:53 -0500

    Testing secure: true for cookie

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ app.use(
     }),
     cookie: {
       maxAge: 1000 * 60 * 10, // 1000 ms/sec * 60 sec/min * 10 min
-      secure: process.env.NODE_ENV === "production", // Enable secure cookies in production
+      secure: false, // Enable secure cookies in production: process.env.NODE_ENV === "production"
       sameSite: "none", // Allow cross-origin requests
       httpOnly: true,
     },
